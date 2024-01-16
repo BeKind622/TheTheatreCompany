d710cf900cd4e2ad5e07873f4903dab18711e97c
BeKind622 <116099798+BeKind622@users.noreply.github.com>
2024-01-16 14:08:35 +0000

    copied myclyde
    change everything homiie

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$hn = "localhost";
+$un = "student_admin";
+$pw = "jeSx@!W1euHa4Kvg";
+$db = "my_clyde_dev";
+
+$conn = new mysqli($hn, $un, $pw, $db);
+
+if ($conn->connect_error){
+    die("connection failed: " . $db->connect_error );
+}
+
+
